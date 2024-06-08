fcbbccb4d30a340e5906d2323fab55d92d2650b4
thai27 <vietthai272000@gmail.com>
2024-06-08 21:06:12 +0700

    test 2

--- src/main/java/com/thai27/trang_tin_tuc_v5_be/Security/WebSecurity.java
+++ src/main/java/com/thai27/trang_tin_tuc_v5_be/Security/WebSecurity.java
@@ -63,7 +63,7 @@ public class WebSecurity {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "ngrok-skip-browser-warning"));
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://trang-tin-tuc-v5-fe.onrender.com/"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://trang-tin-tuc-v5-fe.onrender.com"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
